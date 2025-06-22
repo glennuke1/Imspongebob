da321a6635246ff1f2fbb5b1db9868cb3b14c996
umlei tun <gentpooy@gmail.com>
2025-06-22 22:43:00 +0300

    Add project files.

---
+++ Imspongebob/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace Imspongebob
+{
+    public partial class Form1 : Form
+    {
+        List<string> contents = new List<string>();
+        string title = "Spongebob";
+        string font = "Times New Roman";
+        float titleFont = 30f;
+        float contentFont = 15f;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (File.Exists("config.txt"))
+            {
+                string[] _contents = File.ReadAllLines("config.txt");
+                title = _contents[2].Trim().Split('\t')[0];
+
+                if (!float.TryParse(_contents[3].Trim().Split('\t')[0], out titleFont))
+                {
+                    if (!float.TryParse(_contents[3].Trim().Split(' ')[0], out titleFont))
+                    {
+                        titleFont = 30f;
+                    }
+                }
+
+                if (!float.TryParse(_contents[4].Trim().Split('\t')[0], out contentFont))
+                {
+                    if (!float.TryParse(_contents[4].Trim().Split(' ')[0], out contentFont))
+                    {
+                        contentFont = 30f;
+                    }
+                }
+
+                font = _contents[5].Trim().Split('\t')[0];
+
+                InstalledFontCollection fonts = new InstalledFontCollection();
+                bool fontExists = fonts.Families.Any(f => string.Equals(f.Name, font, StringComparison.OrdinalIgnoreCase));
+
+                if (!fontExists)
+                {
+                    font = "Times New Roman";
+                }
+            }
+            else
+            {
+                title = "Spongebob";
+                titleFont = 30f;
+                contentFont = 15f;
+            }
+
+            if (File.Exists("contents.txt"))
+            {
+                string[] _contents = File.ReadAllLines("contents.txt");
+                for (int i = 0; i < _contents.Length; i++)
+                {
+                    contents.Add(_contents[i]);
+                }
+            }
+
+            var popupNotifier = new PopupNotifier();
+            popupNotifier.TitleText = title;
+            popupNotifier.ContentText = contents[new Random().Next(0, contents.Count)];
+            popupNotifier.IsRightToLeft = false;
+            popupNotifier.TitleFont = new Font(font, titleFont);
+            popupNotifier.ContentFont = new Font(font, contentFont);
+            popupNotifier.Image = Image.FromFile("hqdefault.jpg");
+            popupNotifier.ImageSize = new Size(120, 100);
+            popupNotifier.Delay = 1000000000;
+            popupNotifier.Popup();
+            popupNotifier.Close += PopupNotifier_Close;
+        }
+
+        private void PopupNotifier_Close(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Imspongebob/Program.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace Imspongebob
+{
+    class Program
+    {
+        static Random random = new Random();
+        static System.Media.SoundPlayer player = new System.Media.SoundPlayer("spongebob.wav");
+
+        static int min, max;
+        static string allFonts;
+
+        static void Main(string[] args)
+        {
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            string exePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            rk.SetValue("Spongebob", exePath);
+
+            if (File.Exists("config.txt"))
+            {
+                string[] contents = File.ReadAllLines("config.txt");
+                if (!int.TryParse(contents[0].Trim().Split('\t')[0], out min))
+                {
+                    if (!int.TryParse(contents[0].Trim().Split(' ')[0], out min))
+                    {
+                        min = 60;
+                    }
+                }
+
+                if (!int.TryParse(contents[1].Trim().Split('\t')[0], out max))
+                {
+                    if (!int.TryParse(contents[1].Trim().Split(' ')[0], out max))
+                    {
+                        max = 3600;
+                    }
+                }
+            }
+            else
+            {
+                min = 60;
+                max = 3600;
+            }
+
+            InstalledFontCollection fonts = new InstalledFontCollection();
+            foreach (FontFamily font in fonts.Families)
+            {
+                allFonts += font.Name + "\n";
+            }
+
+            File.WriteAllText("available_fonts.txt", allFonts);
+
+            player.Play();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Form1 form = new Form1();
+            Application.Run(form);
+            Run();
+        }
+
+        static void Run()
+        {
+            Thread.Sleep(random.Next(min * 1000, max * 1000));
+            player.Play();
+            Form1 form = new Form1();
+            Application.Run(form);
+            Run();
+        }
+    }
+}
